4f3c6e0c48ae5813da373b600bfa183732e91f9b
DgDamis <adam.smehyl@seznam.cz>
2017-02-10 11:00:19 +0100

    Ctvrta verze

---
+++ otazky.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Dobrý den."
+for (( i=1; i<=5; i++))
+do
+echo "Zadejte svůj věk."
+read vek
+
+if [ $vek -le 14 ] && [ $vek -gt 0 ]; then
+	echo "Jste dítě."
+fi
+if [ $vek -ge 15 ] && [ $vek -lt 18 ]; then
+	echo "Jste mladistvý."
+fi
+if [ $vek -ge 18 ] && [ $vek -le 150 ]; then
+	echo "Jste dospělý."
+fi
+if [ $vek -ge 150 ]; then
+	echo "To už hniješ v hrobě, no ne? :)"
+fi
+if [ $vek -lt 0 ]; then
+	echo "Opravdu máš mínusový věk? To je neuvěřitelné."
+fi
+done
+
