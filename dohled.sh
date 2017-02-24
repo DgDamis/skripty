a7c7fdddf4439990c341143d4db648b1bf4f2097
Me <you@example.com>
2017-02-24 10:45:40 +0100

    16.0 Upravena podoba html

--- dohled.sh
+++ dohled.sh
@@ -10,14 +10,20 @@ da=`df -h | grep sda | awk -F " " '{printf $4}'`
 um=`free -m | grep Mem: | awk -F " " '{print $3}'`
 fm=`free -m | grep Mem: | awk -F " " '{print $4}'`
 echo "Aktuální čas: "$cas >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "Moje IP: "$ip >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "Přijatá data: "$rx >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "Odeslaná data: "$tx >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "Počet přihlášených uživatelů: "$un >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "Využité místo na disku: "$du >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "Volné místo na disku: "$da >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "Využitá RAM: "$um" MB" >> /home/student/skripty/index.html
+echo "<br>" >> /home/student/skripty/index.html
 echo "volná RAM: "$fm" MB" >> /home/student/skripty/index.html
-echo "<br><br>" >> /home/student/skripty/index.html
-
-
+echo "<br><hr><br>" >> /home/student/skripty/index.html
