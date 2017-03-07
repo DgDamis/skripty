81f03da293429054118a36c267ced1f2592c5722
Me <you@example.com>
2017-03-07 08:38:43 +0100

    Zaloha adresare na jiny pocitac s vypisem hlasky na web. stranku

--- zaloha.sh
+++ zaloha.sh
@@ -2,3 +2,6 @@
 cas=`date +%T`
 zip -r /home/student/skripty/zaloha/smehyl-$cas.zip /home/student/skripty/data
 scp -i /home/student/.ssh/id.rsa /home/student/skripty/zaloha/smehyl-$cas.zip student@192.168.10.91:/home/student/Plocha/zaloha
+echo "Zaloha provedena v "$cas >> /home/student/skripty/zaloha.html
+echo "<br>" >> /home/student/skripty/zaloha.html
+scp -i /home/student/.ssh/id.rsa /home/student/skripty/zaloha.html student@192.168.10.91:/home/student/Plocha/log/smehyl.html
