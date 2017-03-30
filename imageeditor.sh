df2e50917b000f274df36eed90eec07deb237b15
Adam-Virtual-Home <adam.smehyl@seznam.cz>
2017-03-30 19:03:47 +0200

    V 10.5 - ImageEditor.sg - Tweaked 6.0

--- imageeditor.sh
+++ imageeditor.sh
@@ -8,7 +8,7 @@ echo "----------------------------------"
 read choice
 # Rozhodnutí o použizí skriptu pro jeden soubor nebo pro celý adresář
 if [[ $choice -ge '1' && $choice -le '2' ]]; then
-	if [ $choice == 1 ]; then
+	if [ $choice -eq 1 ]; then
 		echo "Zadejte prosím adresu obrázku včetně přípony."
 		read cesta
 		return "$cesta"
@@ -33,8 +33,8 @@ MIN=0
 MAX=360
 echo "Zadejte počet stupňů (0-360) o kolik chcete obrázek otočit."
 read pocetStupnu
-if [ $pocetStupnu -gt $MIN && $pocetStupnu -lt $MAX ] ; then
-	if [ $2 == 2 ]; then
+if [[ $pocetStupnu -gt $MIN && $pocetStupnu -lt $MAX ]] ; then
+	if [ $2 -eq '2' ]; then
 		cd $1
 		# $1 odkazuje na první parametr, který byl funkci dán 
 		for file in $1; do
@@ -55,9 +55,9 @@ echo "Ze kterého formátu a na který formát chcete obrázek převést?"
 echo " 			(1) PNG -------> JPG "
 echo "			(2) JPG -------> PNG "
 read volbaKonverze
-if [ $volbaKonverze -gt 0 && $volbaKonverze -lt 3 ]; then
-	if [ $2 == 2 ]; then
-		if [ $volbaKonverze == 1 ]; then
+if [[ $volbaKonverze -gt '0' && $volbaKonverze -lt '3' ]]; then
+	if [ $2 -eq '2' ]; then
+		if [ $volbaKonverze -eq '1' ]; then
 			cd $1			
 			for file in $1; do
 				nameWend=`$file | awk (".") '{printf $1}'`				
@@ -70,7 +70,7 @@ if [ $volbaKonverze -gt 0 && $volbaKonverze -lt 3 ]; then
 			done
 		fi
 	else
-		if [ $volbaKonverze == 1 ]; then
+		if [ $volbaKonverze -eq '1' ]; then
 			singFilewEnd=`$1 | awk (".") '{printf $1}'`
 			convert $1 $singFilewEnd.jpg
 		else
@@ -90,8 +90,8 @@ MAX=100
 echo "Na kolik procent z původní kvality chcete změnit kvalitu obrázku?"
 echo "Odpovězte ve formátu celého čísla v intervalu (0;100)"
 read kvalita
-if [ $kvalita -gt $MIN &&  $kvalita -lt $MAX ]; then
-	if [ $2 == 2 ]; then
+if [[ $kvalita -gt $MIN &&  $kvalita -lt $MAX ]]; then
+	if [ $2 -eq '2' ]; then
 		cd $1
 		for file in $1; do
 			convert $file -quality $kvalita $file
@@ -112,8 +112,8 @@ read vyska
 echo "Zadejte šířku obrázku, na kterou má být obrázek transformován."
 read width
 celyPomer=`echo $width"x"$vyska`
-if [ [ $width -gt 0 && $width -lt 3841 ] && [ $vyska -gt 0 && $vyska -lt 2161 ] ]; then
-	if [ $2 == 2 ]; then
+if [ [ $width -gt '0' && $width -lt '3841' ] && [ $vyska -gt '0' && $vyska -lt '2161' ] ]; then
+	if [ $2 -eq '2' ]; then
 		cd $1
 		for file in $1; do
 			convert $file -resize $celyPomer $file
@@ -135,19 +135,19 @@ echo " 		(2) Implode			"
 read volbaEfektu
 echo "Zadejte, s jakou hrubostí chcete efekt použít."
 read hrubost
-if [ $volbaEfektu -gt 0 && $volbaEfektu -lt 3 ]; then
+if [ $volbaEfektu -gt '0' && $volbaEfektu -lt '3' ]; then
 	if [ $hrubost -gt 0 ]; then
-		if [ $2 == 2 ]; then
+		if [ $2 -eq 2 ]; then
 			cd $1
 			for file in $1; do
-				if [ $volbaEfektu == 1 ]; then
+				if [ $volbaEfektu -eq '1' ]; then
 					convert $file -charcoal $hrubost $file
 				else
 					convert $file -implode $hrubost $file
 				fi
 			done
 		else
-			if [ $volbaEfektu == 1 ]; then
+			if [ $volbaEfektu -eq '1' ]; then
 				convert $1 -charcoal $hrubost $1
 			else
 				convert $1 -implode $hrubost $1
@@ -175,7 +175,7 @@ echo "(5) Efekty	"
 echo "(6)-------KONEC-------"
 read volba
 
-if [ $volba -gt 0 && $volba -lt 6 ]; then  
+if [[ $volba -gt '0' && $volba -lt '7' ]]; then  
 	case "$volba" in
 	 	1) imageConversion "$cesta" $taskCount
 			;;
